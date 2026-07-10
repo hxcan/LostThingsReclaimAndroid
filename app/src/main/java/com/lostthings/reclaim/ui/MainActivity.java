11a46faf8e8c5bbf74f654ee4b4ca03cce74ea0b
sisterfuture <etakka@gmail.com>
2026-07-10 16:38:52 +0800

    feat: 添加 MainActivity 入口

---
+++ app/src/main/java/com/lostthings/reclaim/ui/MainActivity.java
@@ -0,0 +1,21 @@
+package com.lostthings.reclaim.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lostthings.reclaim.R;
+
+/**
+ * 主 Activity，应用入口。
+ * <p>
+ * 后续子任务会逐步接入 Fragment / 导航 / 登录界面等。
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
